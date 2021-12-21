638b6ab6781542203df770481847295d08ebf191
Marsel Gabbasov <marsel13_98@mail.ru>
2021-12-22 02:41:24 +0300

    header

--- js/index.js
+++ js/index.js
@@ -1,32 +1,4 @@
-function sliderFeedback() {
-    var swiper = new Swiper('.feedback .swiper-container', {
-        slidesPerView: 3,
-        centeredSlidesBounds: false,
-        spaceBetween: 0,
-        navigation: {
-            nextEl: '.feedback .swiper-button-next',
-            prevEl: '.feedback .swiper-button-prev',
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 3,
-                spaceBetween: 0
-            },
-            480: {
-                // slidesPerView: 3,
-                spaceBetween: 0
-            },
-
-        }
-    })
-}
-
-
-
-
 $(document).ready(function() {
-    sliderFeedback()
-
     $(".header__burger-block").click(function() {
         $(".header__right").toggleClass("header__right--active")
         $(".header__burger").toggleClass("header__burger--active")
@@ -37,5 +9,12 @@ $(document).ready(function() {
         $(".header__burger").removeClass("header__burger--active")
     })
 
-
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            console.log(true)
+            $(".header").addClass("header--fixed")
+        } else {
+            $(".header").removeClass("header--fixed")
+        }
+    });
 })
